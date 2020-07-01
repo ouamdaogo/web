ea2837a3988719ac4beaa79784e343555d893649
ouamdaogo <mesdropshippers@gmail.com>
2020-07-01 15:19:12 +0200

    subida en git

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -118,7 +118,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/main.js":[function(require,module,exports) {
-
+var doc = document.getElementById('main');
+doc.innerHTML = 'En espana por via maritimo partido popular';
+console.log(doc);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40017" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43731" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
